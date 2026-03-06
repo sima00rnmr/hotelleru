be54981ba7e3570e24ef16172184be6ccaa1fd6a
sima00rnmr <sima92rnmr@gmail.com>
2026-03-06 18:31:10 +0900

    25章まで終了

--- moattravel/src/main/java/com/example/moattravel/service/UserService.java
+++ moattravel/src/main/java/com/example/moattravel/service/UserService.java
@@ -7,9 +7,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.example.moattravel.entity.Role;
 import com.example.moattravel.entity.User;
 import com.example.moattravel.form.SignupForm;
+import com.example.moattravel.form.UserEditForm;
 import com.example.moattravel.repository.RoleRepository;
 import com.example.moattravel.repository.UserRepository;
-
 @Service
 public class UserService {
 
@@ -42,22 +42,41 @@ public class UserService {
 
 		return userRepository.save(user);
 	}
+	@Transactional
+	public void update(UserEditForm userEditForm) {
+	    User user = userRepository.getReferenceById(userEditForm.getId());
+	    
+	    user.setName(userEditForm.getName());
+	    user.setFurigana(userEditForm.getFurigana());
+	    user.setPostalCode(userEditForm.getPostalCode());
+	    user.setAddress(userEditForm.getAddress());
+	    user.setPhoneNumber(userEditForm.getPhoneNumber());
+	    user.setEmail(userEditForm.getEmail());      
+	    
+	    userRepository.save(user);
+	}    
 
 	// メールアドレスが登録済みかどうかをチェックする
 	public boolean isEmailRegistered(String email) {
-		User user = userRepository.findByEmail(email);
-		return user != null;
+	    User user = userRepository.findByEmail(email);  
+	    return user != null;
 	}
 
 	// パスワードとパスワード（確認用）の入力値が一致するかどうかをチェックする
 	public boolean isSamePassword(String password, String passwordConfirmation) {
-		return password.equals(passwordConfirmation);
-	}
+	    return password.equals(passwordConfirmation);
+	} 
 
 	// ユーザーを有効にする
 	@Transactional
 	public void enableUser(User user) {
-		user.setEnabled(true);
-		userRepository.save(user);
+	    user.setEnabled(true); 
+	    userRepository.save(user);
+	} 
+
+	// メールアドレスが変更されたかどうかをチェックする
+	public boolean isEmailChanged(UserEditForm userEditForm) {
+	    User currentUser = userRepository.getReferenceById(userEditForm.getId());
+	    return !userEditForm.getEmail().equals(currentUser.getEmail());      
+	}  
 	}
-}
