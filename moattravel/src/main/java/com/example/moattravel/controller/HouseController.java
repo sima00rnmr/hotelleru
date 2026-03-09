d58466e384938382a3cbb4326ba0f3332bb073eb
sima00rnmr <sima92rnmr@gmail.com>
2026-03-09 11:09:10 +0900

    33章_終了

--- moattravel/src/main/java/com/example/moattravel/controller/HouseController.java
+++ moattravel/src/main/java/com/example/moattravel/controller/HouseController.java
@@ -1,5 +1,4 @@
 package com.example.moattravel.controller;
-//検索→並び替えに変えるため、それに該当するコード（検索処理）の削除
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,70 +12,71 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.moattravel.entity.House;
+import com.example.moattravel.form.ReservationInputForm;
 import com.example.moattravel.repository.HouseRepository;
 
 @Controller
 @RequestMapping("/houses")
 public class HouseController {
 
-	private final HouseRepository houseRepository;
+    private final HouseRepository houseRepository;        
 
-	public HouseController(HouseRepository houseRepository) {
-		this.houseRepository = houseRepository;
-	}
+    public HouseController(HouseRepository houseRepository) {
+        this.houseRepository = houseRepository;            
+    }     
 
-	@GetMapping
-	public String index(@RequestParam(name = "keyword", required = false) String keyword,
-			@RequestParam(name = "area", required = false) String area,
-			@RequestParam(name = "price", required = false) Integer price,
-			@RequestParam(name = "order", required = false) String order,
-			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
-			Model model) {
-		Page<House> housePage;
-
-		if (keyword != null && !keyword.isEmpty()) {
-			if (order != null && order.equals("priceAsc")) {
-				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByPriceAsc("%" + keyword + "%",
-						"%" + keyword + "%", pageable);
-			} else {
-				housePage = houseRepository.findByNameLikeOrAddressLikeOrderByCreatedAtDesc("%" + keyword + "%",
-						"%" + keyword + "%", pageable);
-			}
-		} else if (area != null && !area.isEmpty()) {
-			if (order != null && order.equals("priceAsc")) {
-				housePage = houseRepository.findByAddressLikeOrderByPriceAsc("%" + area + "%", pageable);
-			} else {
-				housePage = houseRepository.findByAddressLikeOrderByCreatedAtDesc("%" + area + "%", pageable);
-			}
-		} else if (price != null) {
-			if (order != null && order.equals("priceAsc")) {
-				housePage = houseRepository.findByPriceLessThanEqualOrderByPriceAsc(price, pageable);
-			} else {
-				housePage = houseRepository.findByPriceLessThanEqualOrderByCreatedAtDesc(price, pageable);
-			}
-		} else {
-			if (order != null && order.equals("priceAsc")) {
-				housePage = houseRepository.findAllByOrderByPriceAsc(pageable);
-			} else {
-				housePage = houseRepository.findAllByOrderByCreatedAtDesc(pageable);
-			}
-		}
-
-		model.addAttribute("housePage", housePage);
-		model.addAttribute("keyword", keyword);
-		model.addAttribute("area", area);
-		model.addAttribute("price", price);
-		model.addAttribute("order", order);
-
-		return "houses/index";
-	}
-
-	@GetMapping("/{id}")
-	public String show(@PathVariable(name = "id") Integer id, Model model) {
-		House house = houseRepository.getReferenceById(id);
-
-		model.addAttribute("house", house);
-
-		return "houses/show";
-	}
+    @GetMapping
+    public String index(@RequestParam(name = "keyword", required = false) String keyword,
+                        @RequestParam(name = "area", required = false) String area,
+                        @RequestParam(name = "price", required = false) Integer price,  
+                        @RequestParam(name = "order", required = false) String order,
+                        @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+                        Model model) 
+    {
+        Page<House> housePage;
+                
+        if (keyword != null && !keyword.isEmpty()) {            
+            if (order != null && order.equals("priceAsc")) {
+                housePage = houseRepository.findByNameLikeOrAddressLikeOrderByPriceAsc("%" + keyword + "%", "%" + keyword + "%", pageable);
+            } else {
+                housePage = houseRepository.findByNameLikeOrAddressLikeOrderByCreatedAtDesc("%" + keyword + "%", "%" + keyword + "%", pageable);
+            }            
+        } else if (area != null && !area.isEmpty()) {            
+            if (order != null && order.equals("priceAsc")) {
+                housePage = houseRepository.findByAddressLikeOrderByPriceAsc("%" + area + "%", pageable);
+            } else {
+                housePage = houseRepository.findByAddressLikeOrderByCreatedAtDesc("%" + area + "%", pageable);
+            }            
+        } else if (price != null) {            
+            if (order != null && order.equals("priceAsc")) {
+                housePage = houseRepository.findByPriceLessThanEqualOrderByPriceAsc(price, pageable);
+            } else {
+                housePage = houseRepository.findByPriceLessThanEqualOrderByCreatedAtDesc(price, pageable);
+            }            
+        } else {            
+            if (order != null && order.equals("priceAsc")) {
+                housePage = houseRepository.findAllByOrderByPriceAsc(pageable);
+            } else {
+                housePage = houseRepository.findAllByOrderByCreatedAtDesc(pageable);   
+            }            
+        }                
+                
+        model.addAttribute("housePage", housePage);
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("area", area);
+        model.addAttribute("price", price); 
+        model.addAttribute("order", order);
+        
+        return "houses/index";
+    }
+    
+    @GetMapping("/{id}")
+    public String show(@PathVariable(name = "id") Integer id, Model model) {
+        House house = houseRepository.getReferenceById(id);
+        
+        model.addAttribute("house", house);  
+        model.addAttribute("reservationInputForm", new ReservationInputForm());
+        
+        return "houses/show";
+    }    
 }
